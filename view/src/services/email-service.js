533ddd6c197d9b5be14a69c7430134941b652d0f
Moisesmp75 <moisesmore75@gmail.com>
2024-03-13 12:19:23 -0500

    view - add email-service

---
+++ view/src/services/email-service.js
@@ -0,0 +1,31 @@
+const main_url = "http://localhost:3000";
+const url = `${main_url}api/v1/mail`
+
+export class MailService {
+  constructor() {
+    this.requestOption = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+  async search_data(request) {
+    try {
+      this.requestOption["body"] = JSON.stringify(request)
+      const response = await fetch(`${url}/search`, this.requestOption)
+      if(!response.ok)
+        throw new Error(response.status)
+      const { success, message, data, meta } = await response.json()
+      return {
+        success,
+        message,
+        data,
+        meta
+      }
+    } catch (error) {
+      console.log(error)
+      throw error
+    }
+  }
+}
